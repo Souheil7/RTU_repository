615cefb3960cf35be3718f91c4adf2d5517cc200
Souheil7 <90599924+Souheil7@users.noreply.github.com>
2021-09-27 18:32:02 +0300

    Add files via upload

---
+++ ts.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 18:12:09 2021
+
+@author: souhe
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+TS = '''
+{
+"course": 411,
+"courseName": "Software in Telecommunications",
+"releaseYear": 2021,
+"courseActive": true,
+"droppedStudents": null,
+"date": 20210927,
+"data": [[11,2], [22, 4], [33, 1], [44,5]],
+"scores": {"a":77, "b":46, "c":91}
+}
+'''
+
+datab = json.loads(TS)
+
+data = datab.get("data")
+L = len(data)
+
+plt.figure(datab.get("course"))
+plt.title(datab.get("courseName"))
+for i in range(L):
+    x = data[i][0]
+    y = data[i][1]
+    plt.plot(y,x, '.b')
+    
